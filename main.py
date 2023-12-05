36b8fa05d61a99dc70c0875696d86218c5ec84a6
RozovStanislav <116573407+RozovStanislav@users.noreply.github.com>
2023-12-05 19:35:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,7 @@ def parse_all_emails(html_string):
     return re.findall(pattern, html_string)
 
 
-html_string = read_html_file('example.html')
-print("html file:")
-print("------------------------------------")
-print(html_string)
-print("------------------------------------")
-print(parse_all_emails(html_string))
+
 html_string = read_html_file('example.html')
 print("html file:")
 print("------------------------------------")
